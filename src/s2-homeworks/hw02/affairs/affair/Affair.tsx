1fac214e4d12d79d0c36c84d935d744af9afc0c9
kbabiak <kirill.babiak@carrier.com>
2023-11-14 22:34:04 +0100

    HW2

--- src/s2-homeworks/hw02/affairs/affair/Affair.tsx
+++ src/s2-homeworks/hw02/affairs/affair/Affair.tsx
@@ -6,12 +6,12 @@ import s2 from '../Affairs.module.css'
 type AffairPropsType = {
     // key не нужно типизировать
     affair: AffairType
-    deleteAffairCallback: any // need to fix any
+    deleteAffairCallback: (_id: number)=>void // need to fix any
 }
 
 function Affair(props: AffairPropsType) {
     const deleteCallback = () => {
-        // need to fix
+        props.deleteAffairCallback(props.affair._id)
     }
 
     const nameClass = s.name + ' ' + s2[props.affair.priority]
@@ -32,7 +32,7 @@ function Affair(props: AffairPropsType) {
                 {props.affair.priority}
             </div>
 
-            <button
+            <button onClick={deleteCallback}
                 id={'hw2-button-delete-' + props.affair._id}
                 className={buttonClass}
                 // need to fix
