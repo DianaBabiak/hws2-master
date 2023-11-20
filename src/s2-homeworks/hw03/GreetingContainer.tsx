7ceb35c965c8da911f57545fbbb97c30755f9fa1
kbabiak <kirill.babiak@carrier.com>
2023-11-20 22:17:56 +0100

    HW3

--- src/s2-homeworks/hw03/GreetingContainer.tsx
+++ src/s2-homeworks/hw03/GreetingContainer.tsx
@@ -3,18 +3,34 @@ import Greeting from './Greeting'
 import { UserType } from './HW3'
 
 type GreetingContainerPropsType = {
-    users: any // need to fix any
-    addUserCallback: any // need to fix any
+    users: UserType[] // need to fix any
+    addUserCallback: (name:string)=>void // need to fix any
 }
 
-export const pureAddUser = (name: any, setError: any, setName: any, addUserCallback: any) => {
+export const pureAddUser = (name: string, setError: (error:string)=>void, setName: (name:string)=>void, addUserCallback: (name:string)=>void) => {
+   if (name.replace(/\s/g, '')===''){
+       setError("Ошибка! Введите имя!")
+   } else {
+       addUserCallback(name)
+       setName('')
+   }
+
+
     // если имя пустое - показать ошибку, иначе - добавить юзера и очистить инпут
 }
 
 export const pureOnBlur = (name: any, setError: any) => { // если имя пустое - показать ошибку
+    if (name.replace(/\s/g, '')===''){
+        setError("Ошибка! Введите имя!")
+    }
+
 }
 
-export const pureOnEnter = (e: any, addUser: any) => { // если нажата кнопка Enter - добавить
+export const pureOnEnter = (e: {key: string}, addUser: ()=>void) => { // если нажата кнопка Enter - добавить
+    if(e.key === 'Enter'){
+        addUser()
+    }
+
 }
 
 // более простой и понятный для новичков
@@ -26,28 +42,31 @@ const GreetingContainer: React.FC<GreetingContainerPropsType> = ({
     addUserCallback,
 }) => {
     // деструктуризация пропсов
-    const [name, setName] = useState<any>('') // need to fix any
-    const [error, setError] = useState<any>('') // need to fix any
+    const [name, setName] = useState<string>('') // need to fix any
+    const [error, setError] = useState<string>('') // need to fix any
+    const [totalUsers, setTotalUsers]=useState<number>(0)
 
-    const setNameCallback = (e: any) => { // need to fix any
-        setName('some name') // need to fix
+    const setNameCallback = (e:string) => { // need to fix any
+        setName(e) // need to fix
 
         error && setError('')
     }
     const addUser = () => {
         pureAddUser(name, setError, setName, addUserCallback)
+        setTotalUsers(r=>r+1)
+        setName('')
     }
 
     const onBlur = () => {
         pureOnBlur(name, setError)
     }
 
-    const onEnter = (e: any) => {
-        pureOnEnter(e, addUser)
+    const onEnter = (e: string) => {
+        pureOnEnter({key: e}, addUser)
     }
 
-    const totalUsers = 0 // need to fix
-    const lastUserName = 'some name' // need to fix
+
+    const lastUserName = users.length ? users[users.length-1].name :''// need to fix
 
     return (
         <Greeting
