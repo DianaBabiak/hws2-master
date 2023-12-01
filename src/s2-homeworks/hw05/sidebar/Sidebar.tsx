7f2dd0fd534b281bc0a02b96db525b61a208cfd8
kbabiak <kirill.babiak@carrier.com>
2023-12-01 23:32:06 +0100

    hw5

--- src/s2-homeworks/hw05/sidebar/Sidebar.tsx
+++ src/s2-homeworks/hw05/sidebar/Sidebar.tsx
@@ -1,5 +1,5 @@
 import React, {FC} from 'react'
-import {NavLink} from 'react-router-dom'
+import {NavLink, useLocation} from 'react-router-dom'
 import s from './Sidebar.module.css'
 import {PATH} from '../Pages'
 import closeIcon from './closeOutline.svg'
@@ -10,6 +10,8 @@ type PropsType = {
 }
 
 export const Sidebar: FC<PropsType> = ({open, handleClose}) => {
+    const location = useLocation();
+    const currentPath = location.pathname;
     const sidebarClass = s.sidebar
         + (open ? ' ' + s.open : '')
     return (
@@ -31,7 +33,8 @@ export const Sidebar: FC<PropsType> = ({open, handleClose}) => {
                         id={'hw5-pre-junior-link'}
                         to={PATH.PRE_JUNIOR}
                         onClick={handleClose}
-                        // className={...} // делает студент
+                        className={s.link
+                            + (currentPath===PATH.PRE_JUNIOR ? ' ' + s.active : '')}
                     >
                         Pre-junior
                     </NavLink>
@@ -39,7 +42,8 @@ export const Sidebar: FC<PropsType> = ({open, handleClose}) => {
                         id={'hw5-junior-link'}
                         to={PATH.JUNIOR}
                         onClick={handleClose}
-                        // className={...} // делает студент
+                        className={s.link
+                            + (currentPath===PATH.JUNIOR ? ' ' + s.active : '')}
                     >
                         Junior
                     </NavLink>
@@ -47,7 +51,8 @@ export const Sidebar: FC<PropsType> = ({open, handleClose}) => {
                         id={'hw5-junior-plus-link'}
                         to={PATH.JUNIOR_PLUS}
                         onClick={handleClose}
-                        // className={...} // делает студент
+                        className={s.link
+                            + (currentPath===PATH.JUNIOR_PLUS ? ' ' + s.active : '')}
                     >
                         Junior Plus
                     </NavLink>
