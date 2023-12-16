451f728d81224dc3e64707a740a781aa62647cfe
kbabiak <kirill.babiak@carrier.com>
2023-12-16 17:46:57 +0100

    hw7

--- src/s2-homeworks/hw07/common/c6-SuperRadio/SuperRadio.tsx
+++ src/s2-homeworks/hw07/common/c6-SuperRadio/SuperRadio.tsx
@@ -35,7 +35,8 @@ const SuperRadio: React.FC<SuperRadioPropsType> = ({
     ...restProps
 }) => {
     const onChangeCallback = (e: ChangeEvent<HTMLInputElement>) => {
-        // делают студенты
+            onChangeOption?.(parseInt(e.currentTarget.value, 10));
+
     }
 
     const finalRadioClassName = s.radio + (className ? ' ' + className : '')
@@ -48,8 +49,11 @@ const SuperRadio: React.FC<SuperRadioPropsType> = ({
                       id={id + '-input-' + o.id}
                       className={finalRadioClassName}
                       type={'radio'}
-                      // name, checked, value делают студенты
+                      checked={value===o.id}
+                      name={name}
+                      value={o.id}
 
+                      // name, checked, value делают студент
                       onChange={onChangeCallback}
                       {...restProps}
                   />
