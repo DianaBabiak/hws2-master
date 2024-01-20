12979e97e603894890b8978b6f41ae261dd52c1b
kbabiak <kirill.babiak@carrier.com>
2024-01-20 20:17:33 +0100

    hw12

--- src/s2-homeworks/hw12/bll/themeReducer.ts
+++ src/s2-homeworks/hw12/bll/themeReducer.ts
@@ -1,14 +1,26 @@
+export interface StateType{
+    themeId:number
+}
+
+type ActionType=ChangeThemeId
+
+
+
 const initState = {
     themeId: 1,
 }
 
-export const themeReducer = (state = initState, action: any): any => { // fix any
+export const themeReducer = (state:StateType = initState, action: ActionType): StateType => { // fix any
     switch (action.type) {
-        // дописать
+        case "SET_THEME_ID":{
+            return  {...state, themeId:action.id}
+        }
 
         default:
             return state
     }
 }
 
-export const changeThemeId = (id: number): any => ({ type: 'SET_THEME_ID', id }) // fix any
+type ChangeThemeId = ReturnType<typeof changeThemeId>
+export const changeThemeId = (id: number) => ({ type: 'SET_THEME_ID', id }as const) // fix any
+
